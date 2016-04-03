ea6c2aebd28e0200b951b58280f08e1f059953a3
tordog <torie.nielsen@gmail.com>
2016-04-03 15:04:38 -0400

    Add event form UI

--- scheduler-app/AddEventVC.swift
+++ scheduler-app/AddEventVC.swift
@@ -11,12 +11,38 @@ import UIKit
 class AddEventVC: UIViewController {
 
     @IBAction func saveBtnPress(sender: AnyObject) {
+        //save everything to firebase!
+        //under GroupID which I haven't passed yet
+        //should be under groups/groupID/events/date..
+        //title, startTime, endTime, description, numSlots
+        
         self.performSegueWithIdentifier("backToCalendar", sender: nil)
+    }
+    
+    @IBOutlet weak var eventTitle: UITextField!
+    
+    @IBOutlet weak var eventDescription: UITextView!
+    
+    @IBOutlet weak var eventStart: UIDatePicker!
+    
+    @IBOutlet weak var eventEnd: UIDatePicker!
+    
+    @IBOutlet weak var numSlots: UILabel!
+    
+    @IBOutlet weak var stepper: UIStepper!
+    
+    @IBAction func stepperAction(sender: AnyObject) {
+        numSlots.text = "\(Int(stepper.value))"
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let today = NSDate()
+        let endDate = today.dateByAddingTimeInterval(14*60*60*24);
+        eventStart.minimumDate = today
+        eventStart.maximumDate = endDate
+        eventEnd.minimumDate = today
+        eventEnd.maximumDate = endDate
         // Do any additional setup after loading the view.
     }
 
