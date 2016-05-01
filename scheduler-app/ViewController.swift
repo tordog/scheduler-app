dfbf15c73f0f0e65a328e7d3ae3c046ec0562dc9
tordog <torie.nielsen@gmail.com>
2016-05-01 18:25:59 -0400

    UI fixes and keyboard fix

--- scheduler-app/ViewController.swift
+++ scheduler-app/ViewController.swift
@@ -21,7 +21,16 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name:UIKeyboardWillShowNotification, object: nil);
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name:UIKeyboardWillHideNotification, object: nil);
+    }
+    
+    func keyboardWillShow(sender: NSNotification) {
+        self.view.frame.origin.y = -150
+    }
+    
+    func keyboardWillHide(sender: NSNotification) {
+        self.view.frame.origin.y = 0
     }
     
     override func viewDidAppear(animated: Bool) {
